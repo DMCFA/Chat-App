9b303e366fc28149244e0834ac7ffb0bacac2047
Duarte Almeida <duartemalmeida@gmail.com>
2021-06-19 11:49:58 +0100

    add and configure socket.io

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server);
 const port = process.env.PORT
 
 app.use(express.static(__dirname + '/public'));
 
-
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    })
+})
 
 //Server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server listening in port ${port}`);
 })
