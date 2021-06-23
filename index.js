19a3a02408cebbe4d829ec82d24c38fbb0c0e2e1
Duarte Almeida <duartemalmeida@gmail.com>
2021-06-23 14:20:29 +0100

    add the formatMessage object to the io messages

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const path = require('path')
 const express = require('express')
 const http = require('http')
 const socket = require('socket.io')
+const formatMessage = require('./utils/msg')
 
 //Global variables
 const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 const port = process.env.PORT || 3000
+const msgBot = 'ChatDirect'
 
 //Static folder
 app.use(express.static(path.join(__dirname, '/public')));
@@ -18,19 +20,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 io.on('connection', socket => {
     
     //Welcome message to user
-    socket.emit('message', 'Welcome to ChatDirect');
+    socket.emit('message', formatMessage(msgBot, 'Welcome to ChatDirect'))
 
     //Anounce new connected user
-    socket.broadcast.emit('message', 'A user has entered the room')
+    socket.broadcast.emit('message', formatMessage(msgBot, 'A user has entered the room'))
 
     //Anounce disconnected user
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the room')
+        io.emit('message', formatMessage(msgBot, 'A user has left the room'))
     })
 
     //Capture chat messages
     socket.on('chatMessage', msg => {
-        console.log(msg);
+        io.emit('message', formatMessage('USER', msg))
     })
 })
 
