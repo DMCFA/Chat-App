6e83026d426fcb9d6242091bd4b03198cfeabc58
Duarte Almeida <duartemalmeida@gmail.com>
2021-06-23 14:22:11 +0100

    change hardcode values in output message with the data from the url

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,19 @@
 //Global variables
 const socket = io()
 const form = document.getElementById('chat-form')
+const messages = document.querySelector('.chat-messages')
+
+//Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
 
 //Catch Messages
 socket.on('message', message => {
-    outputMessage(message);
+    outputMessage(message)
+
+    //Scroll to bottom
+    messages.scrollTop = messages.scrollHeight
 })
 
 //Submit messages
@@ -16,15 +25,19 @@ form.addEventListener('submit', (e) => {
     
     //Send message to server
     socket.emit('chatMessage', msg)
+
+    //Clear input & focus
+    e.target.elements.message.value = ''
+    e.target.elements.message.focus()
 })
 
-//Send message to the DOM
+//Send message to the DOM function
 const outputMessage = (message) => {
     const div = document.createElement('div')
     div.classList.add('message')
-    div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
 }
